3c36a840ec2deceab848fa5aeee95200ba794225
Leonardo Domingues <leodmgs@gmail.com>
2018-12-16 18:32:34 -0300

    Initial Commit

---
+++ SmartFlip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartFlip
+//
+//  Created by Leonardo Domingues on 12/16/18.
+//  Copyright © 2018 Leonardo Domingues. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
